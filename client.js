be78145f100bf3b7efe4e2ccfbbc240356533544
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-09-13 16:06:13 -0400

    Adding add/join room functionality

--- client.js
+++ client.js
@@ -1,175 +1,18 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-const socket = io("http://localhost:8080");
-
-const BOARD_WIDTH = 700;
-const BOARD_HEIGHT = 700;
-const GRID_SIZE = 20;
-const GRID = []
-const HORIZONTAL_STEP_SIZE = BOARD_WIDTH/GRID_SIZE;
-const VERTICAL_STEP_SIZE = BOARD_HEIGHT/GRID_SIZE;
-const REQUIRED_CONSECUTIVE_PIECES = 5;
-
-var turn = 0;
-var piecesPositions = [];
-
-function getApproximatePosition(x, y) {
-    const HORIZONTAL_STEP_SIZE = BOARD_WIDTH/(GRID_SIZE * 2);
-    const VERTICAL_STEP_SIZE = BOARD_HEIGHT/(GRID_SIZE * 2);
-    let x_pos = Math.ceil(Math.floor(x/HORIZONTAL_STEP_SIZE)/2);
-    let y_pos = Math.ceil(Math.floor(y/VERTICAL_STEP_SIZE)/2);
-    // Handle the case where the x position or y position is 0
-    x_pos = x_pos <= 0 ? 1 : x_pos;
-    y_pos = y_pos <= 0 ? 1 : y_pos;
-    x_pos = x_pos >= GRID_SIZE ? GRID_SIZE - 1 : x_pos;
-    y_pos = y_pos >= GRID_SIZE ? GRID_SIZE - 1 : y_pos;
-    return [x_pos, y_pos];
+function createGame() {
+    console.log('Game created');
+    window.location.replace("http://localhost:8080/create-game");
 }
 
-function getGridIntersection(x, y) {
-    return [x * HORIZONTAL_STEP_SIZE, y * VERTICAL_STEP_SIZE];
-}
-
-function checkPieceExists(x, y) {
-    if(GRID[x][y] == -1) {
-        return false
-    }
-    return true;
-}
-
-function checkVerticalWin(i, j) {
-    if (i < 0 || i + REQUIRED_CONSECUTIVE_PIECES - 1 >= GRID_SIZE || j < 0 || j >= GRID_SIZE) {
-        return false;
-    }
-    if(GRID[i][j] === -1)
-        return false;
-    for(let k=i; k < i + REQUIRED_CONSECUTIVE_PIECES; k++) {
-        if (GRID[k][j] !== GRID[i][j])
-            return false;
-    }
-    return true;
-}
-
-function checkHorizontalWin(i, j) {
-    if (i < 0 || i >= GRID_SIZE || j < 0 || j + REQUIRED_CONSECUTIVE_PIECES - 1 >= GRID_SIZE) {
-        return false;
-    }
-    if(GRID[i][j] === -1)
-        return false;
-    for(let k=j; k < j + REQUIRED_CONSECUTIVE_PIECES; k++) {
-        if (GRID[i][k] !== GRID[i][j])
-            return false
-    }
-    return true;
-}
-
-function checkDiagonalWin(i, j) {
-    if (i < 0 || i + REQUIRED_CONSECUTIVE_PIECES - 1 >= GRID_SIZE || j < 0 || j + REQUIRED_CONSECUTIVE_PIECES - 1 >= GRID_SIZE) {
-        return false;
-    }
-    if(GRID[i][j] === -1)
-        return false;
-    for(let k=0; k < REQUIRED_CONSECUTIVE_PIECES; k++) {
-        if (GRID[i+k][j+k] !== GRID[i][j])
-            return false
-    }
-    return true;
-}
-
-function checkReverseDiagonalWin(i, j) {
-    if (i < REQUIRED_CONSECUTIVE_PIECES - 1 || i >= GRID_SIZE || j < 0 || j + REQUIRED_CONSECUTIVE_PIECES - 1 >= GRID_SIZE) {
-        return false;
-    }
-    if(GRID[i][j] === -1)
-        return false;
-    for(let k=0; k < REQUIRED_CONSECUTIVE_PIECES; k++) {
-        if (GRID[i-k][j+k] !== GRID[i][j])
-            return false
-    }
-    return true;
-}
-
-function checkWinner() {
-    // Turn is incremented after every move. Initially turn is 0.
-    if(turn < 8) {
-        return false
-    }
-    for (let i=0; i < GRID_SIZE; i++) {
-        for(let j=0; j < GRID_SIZE; j++) {
-            if (checkHorizontalWin(i, j) || checkVerticalWin(i, j) || checkDiagonalWin(i, j) || checkReverseDiagonalWin(i, j)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function updateGrid(x, y, emit = false) {
-    let gridApproxPosition = getGridIntersection(y, x);
-    if (!checkPieceExists(x, y)) {
-        const canvas = document.getElementById('board');
-        const ctx = canvas.getContext('2d');
-        ctx.beginPath();
-        ctx.arc(gridApproxPosition[0], gridApproxPosition[1], HORIZONTAL_STEP_SIZE/2, 0, 2 * Math.PI, false);
-        ctx.fillStyle = turn % 2 === 0 ? 'black' : 'white';
-        ctx.fill();
-        GRID[x][y] = turn % 2;
-        if(emit)
-            socket.emit('move', {x: x, y: y, turn: turn});
-        if(checkWinner()) {
-            alert('Player with ' + (turn % 2 === 0 ? 'black' : 'white') + ' pieces wins!');
-        }
-        turn++;
-        const playerInfo = document.getElementById('player-info');
-        playerInfo.innerHTML = 'Player with ' + (turn % 2 === 0 ? 'black' : 'white') + ' pieces turn';
-        
-    } else {
-        alert('Piece already exists at this position.\nPlesae choose another position.');
-    }
-}
-
-function addPiece(event) {
-    const canvas = document.getElementById('board');
-    const canvasTop = canvas.offsetTop;
-    const canvasLeft = canvas.offsetLeft;
-    const x = event.pageX - canvasLeft;
-    const y = event.pageY - canvasTop;
-    let approximatePosition = getApproximatePosition(x, y);
-    updateGrid(approximatePosition[1], approximatePosition[0], true);
-}
-
-function initializeBoard() {
-    const canvas = document.getElementById('board');
-    const ctx = canvas.getContext('2d');
-    const padding = 0;
-
-    const HORIZONTAL_STEP_SIZE = BOARD_WIDTH/GRID_SIZE;
-    const VERTICAL_STEP_SIZE = BOARD_HEIGHT/GRID_SIZE;
-    for (let x = HORIZONTAL_STEP_SIZE; x <= BOARD_WIDTH; x += HORIZONTAL_STEP_SIZE) {
-        ctx.moveTo(0.5 + x + padding, padding);
-        ctx.lineTo(0.5 + x + padding, BOARD_HEIGHT + padding);
-    }
-
-    for (let x = VERTICAL_STEP_SIZE; x <= BOARD_HEIGHT; x += VERTICAL_STEP_SIZE) {
-        ctx.moveTo(padding, 0.5 + x + padding);
-        ctx.lineTo(BOARD_WIDTH + padding, 0.5 + x + padding);
-    }
-
-    for(let i=0; i<GRID_SIZE; i++) {
-        GRID.push([]);
-        for(let j=0; j<GRID_SIZE; j++) {
-            GRID[i].push(-1);
-        }
-    }
-
-    ctx.strokeStyle = "black";
-    ctx.stroke();
-    canvas.addEventListener('click', addPiece, false);
+function joinGame() {
+    console.log('Game joined');
+    const gameCode = document.getElementById('game-code').value;
+    console.log('Game code: ' + gameCode);
+    window.location.replace("http://localhost:8080/" + gameCode);
 }
 
 window.onload = function() {
-    initializeBoard();
+    const createGameButton = document.getElementById('create-game');
+    createGameButton.addEventListener('click', createGame, false);
+    const joinGameButton = document.getElementById('join-game');
+    joinGameButton.addEventListener('click', joinGame, false);
 }
-
-socket.on('move', function(data) {
-    updateGrid(data.x, data.y);
-});
--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http')
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 8080 // setting the port
 let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 let server = http.createServer(app)
 let io = socketIO(server)
+
+const gameCodeSet = new Set();
+const gameCodeToStateMap = new Map();
+const socketToGameCodeMap = new Map();
+const socketToPlayerIdMap = new Map();
+const gameCodeToPlayers = new Map();
+const gameCodeSocketToTurnMap = new Map();
+const GRID_SIZE = 20;
+
+function makeUniqueGameIds(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+function createGrid() {
+    let grid = [];
+    for(let i=0; i<GRID_SIZE; i++) {
+        grid.push([]);
+        for(let j=0; j<GRID_SIZE; j++) {
+            grid[i].push(-1);
+        }
+    }
+    return grid;
+}
+
+function getGameStateByGameCode(gameCode) {
+    return gameCodeToStateMap.get(gameCode);
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -15,6 +52,10 @@ app.get('/client.css', (req, res) => {
     res.sendFile(__dirname + '/client.css')
 })
 
+app.get('/game.js', (req, res) => {
+    res.sendFile(__dirname + '/game.js')
+})
+
 app.get('/client.js', (req, res) => {
     res.sendFile(__dirname + '/client.js')
 })
@@ -22,6 +63,47 @@ app.get('/client.js', (req, res) => {
 app.get('/favicon.ico', (req, res) => {
     res.sendFile(__dirname + '/favicon.ico')
 })
+
+app.get('/create-game', (req, res) => {
+    const gameCode = makeUniqueGameIds(5);
+    const grid = createGrid();
+    const gameState = {
+        grid: grid,
+        turn: 0,
+        numPlayers: 0,
+        players: [],
+        nextPlayerTurn: 1,
+    };
+    gameCodeSet.add(gameCode);
+    gameCodeToStateMap.set(gameCode, gameState);
+    res.redirect(`/${gameCode}`);
+})
+
+app.get('/:gameCode', (req, res) => {
+    const gameCode = req.params.gameCode;
+    if(gameCodeSet.has(gameCode)) {
+        if(gameCodeToPlayers.has(gameCode) && gameCodeToPlayers.get(gameCode).size >= 2) {
+            res.send('Game is full');
+        } else {
+            res.sendFile(__dirname + '/game.html')
+        }
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+app.post('/get-game-state', (req, res) => {
+    const gameCode = req.body.gameCode;
+    if(gameCodeSet.has(gameCode)) {
+        const gameState = getGameStateByGameCode(gameCode);
+        console.log('Game state: ' + gameState);
+        res.send(gameState.grid);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/join-game', (req, res) => {})
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
@@ -35,6 +117,45 @@ io.on('connection', (socket) => {
     })
 
     socket.on('move', (data) => {
-        socket.broadcast.emit('move', data);
+        console.log(data)
+        console.log('Game code: ' + data.gameCode);
+        const gameState = getGameStateByGameCode(data.gameCode);
+        console.log('Game state: ' + gameState);
+        gameState.grid[data.x][data.y] = data.playerId;
+        socket.broadcast.emit('move', {x: data.x, y: data.y});
+    })
+
+    socket.on('initialize', (data) => {
+        console.log('Initializing game', data);
+        if (socketToGameCodeMap.has(socket.id)) {
+            const gameCodes = socketToGameCodeMap.get(socket.id);
+            if (gameCodes.has(data.gameCode)) {
+                return;
+            } else {
+                gameCodes.add(data.gameCode);
+            }
+        } else {
+            const gameCodes = new Set();
+            gameCodes.add(data.gameCode);
+            socketToGameCodeMap.set(socket.id, gameCodes);
+        }
+        if (gameCodeToPlayers.has(data.gameCode)) {
+            const players = gameCodeToPlayers.get(data.gameCode);
+            players.add(socket.id);
+            gameCodeSocketToTurnMap.set({gameCode: data.gameCode, socket: socket.id}, players.size - 1);
+        } else {
+            const players = new Set();
+            players.add(socket.id);
+            gameCodeSocketToTurnMap.set({gameCode: data.gameCode, socket: socket.id}, players.size - 1);
+            gameCodeToPlayers.set(data.gameCode, players);
+        }
+        if(gameCodeToStateMap.has(data.gameCode)) {
+            const gameState = getGameStateByGameCode(data.gameCode);
+            gameState.numPlayers++;
+            gameState.players.push(socket.id);
+            // socket.broadcast.emit('initialize', {playerId: gameState.numPlayers - 1});
+        }
+        console.log(data);
+        console.log(socketToGameCodeMap);
     })
 })
